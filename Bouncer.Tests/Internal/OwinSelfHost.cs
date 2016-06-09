b4ba856c32b63e5dcaf8130b7503af9211a9970d
Cillié Malan <cilliemalan@gmail.com>
2016-06-09 19:48:56 +0200

    Added self hosting for tests

--- Bouncer.Tests/Internal/OwinSelfHost.cs
+++ Bouncer.Tests/Internal/OwinSelfHost.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Hosting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,50 @@ using System.Threading.Tasks;
 
 namespace Bouncer.Tests.Internal
 {
-    class OwinSelfHost
+    using System.Threading;
+    using AppFunc = Func<IOwinContext, Func<Task>, Task>;
+
+    public class OwinSelfHost : IDisposable
     {
+        private static object _syncroot = new object();
+        private static int _port = 21002;
+        private IDisposable _runningApi;
+        public string BaseAddress { get; set; }
+
+
+        public OwinSelfHost(params AppFunc[] additionalMiddleware)
+        {
+            if (_runningApi != null) throw new InvalidOperationException("The API is already started.");
+
+            if(string.IsNullOrEmpty(BaseAddress))
+            {
+                BaseAddress = $"http://localhost:{Interlocked.Increment(ref _port)}/";
+            }
+
+            lock (_syncroot)
+            {
+                Startup.AdditionalMiddleware.Clear();
+                Startup.AdditionalMiddleware.AddRange(additionalMiddleware);
+                _runningApi = WebApp.Start<Startup>(BaseAddress);
+                Startup.AdditionalMiddleware.Clear();
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (_runningApi != null)
+                {
+                    _runningApi.Dispose();
+                    _runningApi = null;
+                }
+            }
+        }
     }
 }
---
+++ Bouncer.Tests/Internal/Startup.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Bouncer.Tests.Internal
+{
+    using AppFunc = Func<IOwinContext, Func<Task>, Task>;
+    internal class Startup
+    {
+        public static List<AppFunc> AdditionalMiddleware { get; } = new List<AppFunc>();
+        public static BouncerManager BouncerManager { get; set; }
+
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseErrorPage();
+            app.UseBouncer(BouncerManager ?? new BouncerManager());
+            app.UseWelcomePage("/");
+            AdditionalMiddleware.ForEach(mw => app.Use(mw));
+        }
+    }
+}
