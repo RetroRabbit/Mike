dce0b4197ec0dddf75af86c2ee7a43e27582c741
Cillié Malan <cilliemalan@gmail.com>
2016-06-09 16:47:54 +0200

    Added BouncerMiddleware and BouncerExtensions containing extensions that can use it

---
+++ Bouncer.Owin/BouncerMiddleware.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bouncer.Owin
+{
+    using AppFunc = Func<IDictionary<string, object>, Task>;
+
+    internal class BouncerMiddleware
+    {
+        private BouncerManager _bouncer;
+        private Func<IDictionary<string, object>, Task> _next;
+
+        public BouncerMiddleware(AppFunc next, BouncerManager bouncer)
+        {
+            _bouncer = bouncer;
+            _next = next;
+        }
+
+        public async Task Invoke(IDictionary<string, object> environment)
+        {
+            var request = new OwinRequestContext(environment);
+            var report = _bouncer.Analyze(request);
+            if (report.ActionRequired)
+            {
+                bool mustStillInvokeNext = await _bouncer.ActionOnReportAsync(report, new OwinPlatform(environment));
+                if(mustStillInvokeNext)
+                {
+                    await _next(environment);
+                }
+            }
+            else
+            {
+                await _next(environment);
+            }
+        }
+    }
+}
