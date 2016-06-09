a81a3b5940206674bd801f9aaf03afd547340ce4
Cillié Malan <cilliemalan@gmail.com>
2016-06-09 20:48:44 +0200

    Added NominalIpAddressTest, IpAddressRewriteTest, and IpAddressRewriteLocalTest

--- Bouncer.Tests/BouncerSelfHostTests.cs
+++ Bouncer.Tests/BouncerSelfHostTests.cs
@@ -32,5 +32,65 @@ namespace Bouncer.Tests
                 }
             }
         }
+
+        [Fact]
+        public async Task NominalIpAddressTest()
+        {
+            using (var host = new OwinSelfHost())
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    using (var response = await client.GetAsync($"{host.BaseAddress}ipaddress"))
+                    {
+                        var content = await response.Content.ReadAsStringAsync();
+
+                        Assert.True(response.IsSuccessStatusCode);
+                        Assert.Equal("::1", content);
+                    }
+                }
+            }
+        }
+
+        [Fact]
+        public async Task IpAddressRewriteTest()
+        {
+            using (var host = new OwinSelfHost())
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    var request = new HttpRequestMessage(HttpMethod.Get, $"{host.BaseAddress}ipaddress");
+                    request.Headers.Add("X-Forwarded-For", "196.32.12.33");
+
+                    using (var response = await client.SendAsync(request))
+                    {
+                        var content = await response.Content.ReadAsStringAsync();
+
+                        Assert.True(response.IsSuccessStatusCode);
+                        Assert.Equal("196.32.12.33", content);
+                    }
+                }
+            }
+        }
+
+        [Fact]
+        public async Task IpAddressRewriteLocalTest()
+        {
+            using (var host = new OwinSelfHost())
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    var request = new HttpRequestMessage(HttpMethod.Get, $"{host.BaseAddress}ipaddress");
+                    request.Headers.Add("X-Forwarded-For", "172.27.12.33");
+
+                    using (var response = await client.SendAsync(request))
+                    {
+                        var content = await response.Content.ReadAsStringAsync();
+
+                        Assert.True(response.IsSuccessStatusCode);
+                        Assert.Equal("::1", content);
+                    }
+                }
+            }
+        }
     }
 }
