e4dd86fa566d96085f1e8bf49896bea829c4d21b
Cillié Malan <cilliemalan@gmail.com>
2016-06-09 16:47:06 +0200

    Added OwinPlatform

---
+++ Bouncer.Owin/OwinPlatform.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Bouncer.Owin
+{
+    internal class OwinPlatform : IPlatform
+    {
+        private IDictionary<string, object> _environment;
+
+        public OwinPlatform(IDictionary<string, object> environment)
+        {
+            _environment = environment;
+        }
+
+        public void RewriteRemoteIpAddress(string newAddress)
+        {
+            _environment["server.RemoteIpAddress"] = newAddress;
+
+            object httpcontext = _environment["System.Web.HttpContextBase"];
+            if (httpcontext != null)
+            {
+                SetNewRemoteIpAddressForHttpContext(newAddress, httpcontext);
+            }
+        }
+
+        public Task RewriteResponseAsync(IResponse newResponse, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+        
+        private static void SetNewRemoteIpAddressForHttpContext(string newAddress, object httpcontext)
+        {
+            try
+            {
+                //TODO: this is slow
+                var context = httpcontext.GetType().GetProperty("Current")?.GetValue(null);
+
+                if (context != null)
+                {
+                    var request = context.GetType().GetProperty("Request")?.GetValue(context);
+                    if (request != null)
+                    {
+                        var serverVariables = request.GetType().GetProperty("ServerVariables")?.GetValue(request) as NameValueCollection;
+                        if (serverVariables != null)
+                        {
+                            serverVariables["REMOTE_ADDR"] = newAddress;
+                            serverVariables["REMOTE_HOST"] = newAddress;
+                        }
+                    }
+                }
+            }
+            catch { }
+        }
+    }
+}
