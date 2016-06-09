e91e3ba97af7fb79204043d6f25d042d5750d94e
Cillié Malan <cilliemalan@gmail.com>
2016-06-09 14:29:41 +0200

    Added BouncerModule

---
+++ Bouncer.SystemWeb/BouncerModule.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Bouncer.SystemWeb
+{
+    public class BouncerModule : IHttpModule
+    {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Init(HttpApplication application)
+        {
+            application.BeginRequest += Application_BeginRequest;
+        }
+
+        private void Application_BeginRequest(object sender, EventArgs e)
+        {
+        }
+    }
+}
