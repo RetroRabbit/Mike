a5c80247712955fc76bfbf7504846a49afc13f7f
Cillié Malan <cilliemalan@gmail.com>
2016-06-09 16:46:32 +0200

    Added Bouncer.Owin HelperExtensions

---
+++ Bouncer.Owin/HelperExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bouncer.Owin
+{
+    internal static class HelperExtensions
+    {
+        public static T Get<T>(this IDictionary<string, object> dictionary, string key)
+        {
+            object o;
+            if (dictionary.TryGetValue(key, out o))
+            {
+                if (o is T) return (T)o;
+            }
+            return default(T);
+        }
+    }
+}
