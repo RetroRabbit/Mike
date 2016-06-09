d030cb55dff0d44ce3d12712f98b737bc5f7d01e
Cillié Malan <cilliemalan@gmail.com>
2016-06-09 16:41:58 +0200

    Added OwinRequestContext

---
+++ Bouncer.Owin/OwinRequestContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bouncer.Owin
+{
+    public class OwinRequestContext : IRequestContext
+    {
+        private IDictionary<string, object> _environment;
+
+        public IDictionary<string, string[]> Headers { get { return _environment.Get<IDictionary<string, string[]>>("owin.RequestHeaders"); } }
+        public string Method { get { return _environment.Get<string>("owin.RequestMethod"); } }
+        public string Path { get { return GetPath(); } }
+        public string QueryString { get { return _environment.Get<string>("owin.RequestQueryString"); } }
+        public string Scheme { get { return _environment.Get<string>("owin.RequestScheme"); } }
+        public string RemoteIpAddress { get { return _environment.Get<string>("server.RemoteIpAddress"); } }
+        public string LocalIpAddress { get { return _environment.Get<string>("server.LocalIpAddress"); } }
+
+        public OwinRequestContext(IDictionary<string, object> environment)
+        {
+            _environment = environment;
+        }
+
+        private string GetPath()
+        {
+            var path = $"{_environment.Get<string>("owin.RequestPathBase")}{_environment.Get<string>("owin.RequestPath")}";
+
+            var qs = _environment.Get<string>("owin.RequestQueryString");
+            if (!string.IsNullOrEmpty(qs)) path += "?" + qs;
+
+            return path;
+        }
+
+        private string GetUrl()
+        {
+            var headers = _environment.Get<IDictionary<string, string[]>>("owin.RequestHeaders");
+
+            return $"{_environment.Get<string>("owin.RequestScheme")}://{headers?["Host"]?.FirstOrDefault() ?? "<unknown>"}{GetPath()}";
+        }
+    }
+}
