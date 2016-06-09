1c0378ba44b7d819870e5fe2bd8c2c496105c961
Cillié Malan <cilliemalan@gmail.com>
2016-06-09 23:07:23 +0200

    Added SystemWebPlatform, SystemWebRequest

---
+++ Bouncer.SystemWeb/SystemWebPlatform.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Linq;
+
+namespace Bouncer.SystemWeb
+{
+    internal class SystemWebPlatform : IPlatform
+    {
+        private HttpContext _context;
+
+        public SystemWebPlatform(HttpContext context)
+        {
+            _context = context;
+        }
+
+        public void RewriteRemoteIpAddress(string newAddress)
+        {
+            _context.Request.ServerVariables["REMOTE_ADDR"] = newAddress;
+            _context.Request.ServerVariables["REMOTE_HOST"] = newAddress;
+        }
+
+        public async Task RewriteResponseAsync(IResponse newResponse, CancellationToken cancellationToken)
+        {
+            var response = _context.Response;
+
+            response.ClearHeaders();
+            foreach (var header in newResponse.Headers) response.Headers.Add(header.Key, header.Value);
+            response.StatusCode = newResponse.StatusCode;
+            response.Status = newResponse.ReasonPhrase;
+            await newResponse.ResponseBody.CopyToAsync(response.OutputStream, 1024, cancellationToken);
+        }
+    }
+}
---
+++ Bouncer.SystemWeb/SystemWebRequest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Linq;
+
+namespace Bouncer.SystemWeb
+{
+    internal class SystemWebRequest : IRequestContext
+    {
+        private HttpRequest _request;
+
+        public SystemWebRequest(HttpRequest request)
+        {
+            _request = request;
+        }
+
+        public IDictionary<string, string[]> Headers
+        {
+            get
+            {
+                return _request.Headers.Keys
+                    .Cast<string>()
+                    .ToDictionary(x => x, x => new string[] { _request.Headers[x].ToString() });
+            }
+        }
+
+        public string LocalIpAddress { get { return null; } }
+        public string Method { get { return _request.HttpMethod; } }
+        public string Path { get { return _request.Path; } }
+        public string QueryString { get { return _request.Url.Query; } }
+        public string RemoteIpAddress { get { return _request.UserHostAddress; } }
+        public string Scheme { get { return _request.Url.Scheme; } }
+    }
+}
