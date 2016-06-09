fd6ab33ae7375cb75de6018863027428df732ebe
Cillié Malan <cilliemalan@gmail.com>
2016-06-09 16:46:03 +0200

    Added Bouncer HelperExtensions

---
+++ Bouncer/HelperExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bouncer
+{
+    internal static class HelperExtensions
+    {
+        public static string GetHeader(this IRequestContext request, string header)
+        {
+            return request.Headers?[header]?.FirstOrDefault();
+        }
+    }
+}
